654fb79e3841fc85d6b483c091de38d7c9215769
ycarr5150 <ycarrillo@csumb.edu>
2019-10-15 02:33:30 +0000

    Pictures Upload

--- public/labs/lab5_pixabay/js/functions.js
+++ public/labs/lab5_pixabay/js/functions.js
@@ -18,7 +18,12 @@ function searchForImages() {
         url: "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&" + searchString,
         dataType: "json",
         success: function(result, status) {
-            console.log(result); 
+            for (let i = 0; i < 4; i++) {
+                $("#likes").append(`Likes: ${result.hits[i].likes} &emsp;&emsp;&emsp;&emsp; &emsp;&emsp;`); 
+                $("#pictures").append(` <img src="${result.hits[i].largeImageURL}" 
+                    alt="picture" width="${result.hits[i].previewWidth}" height="${result.hits[i].previewHeight}"
+                    style="border-radius: 25px;"> `); 
+            }
     }
     }); 
 }
