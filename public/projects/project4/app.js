0c7ddba3b7110ab998a65271f661ef6aa771acdf
ycarr5150 <ycarrillo@csumb.edu>
2019-10-31 19:05:24 +0000

    opening port for heruko

--- public/projects/project4/app.js
+++ public/projects/project4/app.js
@@ -10,11 +10,24 @@ app.get("/", function(req, res) {
 });
 
 
-// server listener 
+app.get("/resume", function(req, res) {
+    res.render("resume.html"); 
+});
+
+app.get("/about", function(req, res) {
+    res.render("about.html"); 
+});
+
+app.get("/", function(req, res) {
+    res.render("contact.html"); 
+});
+
+
+// // server listener 
 app.listen("8081", "127.0.0.1", function(){
     console.log("Express Server is Running ..."); 
 }); 
 
-// app.listen(process.env.PORT, process.env.IP, function() {
-//     console.log("Running Express Server..."); 
-// });
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Running Express Server..."); 
+});
