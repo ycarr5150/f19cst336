615bacc1c192a7e868102311e9bdc0146c0c43b3
ycarr5150 <ycarrillo@csumb.edu>
2019-11-18 04:39:27 +0000

    fixing url

--- public/projects/project4/router.js
+++ public/projects/project4/router.js
@@ -4,25 +4,25 @@ var faker = require('faker');
 
 // routes
 router.get("/", function(req, res) {
-    res.render("../public/projects/project4a/views/index.html", {
+    res.render("../public/projects/project4/views/index.html", {
         "quote": "I'm looking to expand my portfolio while I'm on top and I'm young.", 
         "person": "- Floyd Mayweather Jr."
     }); 
 });
 
 router.get("/about", function(req, res) {
-    res.render("../public/projects/project4a/views/about.html", {
+    res.render("../public/projects/project4/views/about.html", {
         "about1" : faker.lorem.paragraphs(),
         "about2" : faker.lorem.paragraphs()
     }); 
 });
 
 router.get("/resume", function(req, res) {
-    res.render("../public/projects/project4a/views/resume.html"); 
+    res.render("../public/projects/project4/views/resume.html"); 
 });
 
 router.get("/contact", function(req, res) {
-    res.render("../public/projects/project4a/views/contact.html"); 
+    res.render("../public/projects/project4/views/contact.html"); 
 });
 
 module.exports = router;
