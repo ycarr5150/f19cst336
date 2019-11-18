06b10143427e1733c96155121c7837cd8ba83d18
ycarr5150 <ycarrillo@csumb.edu>
2019-11-18 02:20:57 +0000

    updating onload images

--- public/labs/lab5_pixabay/js/functions.js
+++ public/labs/lab5_pixabay/js/functions.js
@@ -11,48 +11,8 @@ let rSNumber = Math.floor(Math.random() * 5);
 console.log(rSNumber);
 console.log(randomSearch[rSNumber]);
 
-$.ajax({
-    method: "GET",
-    url: "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q=" + randomSearch[rSNumber],
-    dataType: "json",
-    success: function(result, status) {
-        totalHits = result.hits.length; // how many search results returned 
-
-        if (totalHits == 0) {
-            $("#error-message").html("There are no search results available.<br>Please change your search.").css("color", "red");
-        }
-        else {
-            $("#display").html("");
-            $("#error-message").html("");
-
-            var randomNumber = new Array();
-
-            while (randomNumber.length != 4) {
-                let previous = false;
-                let number = Math.floor(Math.random() * totalHits);
-
-                for (let i = 0; i < randomNumber.length; i++) {
-                    if (number == randomNumber[i]) {
-                        previous = true;
-                    }
-                }
-
-                if (!previous) {
-                    randomNumber.push(number);
-                }
-            }
-
-            // displaying picture 
-            for (let i = 0; i < 4; i++) {
-                $("#display").append(`<div style="width: ${result.hits[randomNumber[i]].previewWidth}"> 
-                        Likes: ${result.hits[randomNumber[i]].likes} <br><br> 
-                        <img src="${result.hits[randomNumber[i]].largeImageURL}" alt="picture" 
-                        width="${result.hits[randomNumber[i]].previewWidth}" height="${result.hits[randomNumber[i]].previewHeight}" 
-                        style="border-radius: 25px;"> </div>`);
-            }
-        }
-    } // success
-}); // ajax
+searchString = "q=" + randomSearch[rSNumber]; 
+searchForImages(); 
 
 $("#search-button").on("click", function() {
     keyword = $("#keyword").val();
